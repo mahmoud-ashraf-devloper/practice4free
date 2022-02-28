5dbe73f2183543dee4ea01cf3e1c16ae7a858114
unknown <mahmoud-ashraf-developer>
2022-02-28 02:26:44 +0200

    Room Page

---
+++ app/Http/Controllers/RoomController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class RoomController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Room',[
+            'data'
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\GoogleSocialiteController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RoomController;
 use App\Models\User;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Auth;
@@ -34,6 +35,7 @@ Route::get('/', function () {
 //})->name('dashboard');
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/room/{roomId}', [RoomController::class, 'index'])->name('room');
 
 Route::get('auth/google', [GoogleSocialiteController::class, 'redirectToGoogle']);
 Route::get('callback/google', [GoogleSocialiteController::class, 'handleCallback']);
